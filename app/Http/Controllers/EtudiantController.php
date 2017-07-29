fd66b2dd7e6b1d5e5c5c13c4086329825c10218e
Hares Faten <faten.hrs1994@gmail.com>
2017-07-29 15:29:47 +0100

    1

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Abbes
- * Date: 29/07/2017
- * Time: 12:00
- */
+
 
 namespace App\Http\Controllers;
 
@@ -78,44 +73,52 @@ class EtudiantController extends Controller
         }
 
 
-        if ($this->etudiantRepository->addEtudiant($hopitalId, $serviceId, $enseignantId, $startDate, $endDate)) {
-            return response()->json(["message" => "add stage success"], 200);
+
+
+
+        if ($this->etudiantRepository->addEtudiant($niveauId,$competencesId,$nom,$prenom,$CIN,$carte_Etudiant,$email,$active,$confirmation_code,$qr_code)) {
+            return response()->json(["message" => "add etudiant success"], 200);
         } else {
-            return response()->json(["error" => "cannot add stage"], 500);
+            return response()->json(["error" => "cannot add etudiant"], 500);
         }
     }
 
 
-    public function updateStage($stageId, Request $request)
+    public function updateEtudiant($etudianteId, Request $request)
     {
         if (!$request->has([
-            "hopitalId",
-            "serviceId",
-            "enseignantId",
-            "startDate",
-            "endDate"
-        ])
-        ) {
-            return response()->json(["error" => "Bad Request"], 400);
-        }
-        $hopitalId = $request->input("hopitalId");
-        $serviceId = $request->input("serviceId");
-        $enseignantId = $request->input("enseignantId");
-        $startDate = $request->input("startDate");
-        $endDate = $request->input("endDate");
-        if (!$this->hopitalRepository->isExistHopital($hopitalId)) {
-            return response()->json(["error" => "hospital not found"], 404);
-        }
-        if (!$this->serviceRepository->isExistService($serviceId)) {
-            return response()->json(["error" => "service not found"], 404);
-        }
-        if (!$this->enseignantRepository->isExistEnseignant($enseignantId)) {
-            return response()->json(["error" => "enseigant not found"], 404);
-        }
+          "niveauId",
+          "competencesId",
+          "nom",
+          "prenom",
+          "CIN",
+          "carte_Etudiant",
+          "email",
+          "active",
+          "confirmation_code",
+          "qr_code"
 
-        if (!$this->stageRepository->isExistStage($stageId)) {
-            return response()->json(["error" => "stage not found"], 404);
-        }
+      ])
+      ) {
+          return response()->json(["error" => "Bad Request"], 400);
+      }
+
+      $niveauId = $request->input("niveauId");
+      $competencesId = $request->input("competencesId");
+      $nom = $request->input("nom");
+      $prenom = $request->input("prenom");
+      $CIN = $request->input("CIN");
+        $carte_Etudiant = $request->input("carte_Etudiant");
+        $email = $request->input("email");
+        $active= $request->input("active");
+        $confirmation_code = $request->input("confirmation_code");
+        $qr_code = $request->input("qr_code");
+      if (!$this->niveauRepository->isExistNiveau($niveauId)) {
+          return response()->json(["error" => "niveau not found"], 404);
+      }
+      if (!$this->competencesRepository->isExistCompetences($competencesId)) {
+          return response()->json(["error" => "competences not found"], 404);
+      }
 
         if ($this->etudiantRepository->updateEtudiant($etudiantId, $niveauId ,$competencesId ,$nom ,$prenom ,$CIN ,$carte_Etudiant ,$email ,$active,$confirmation_code,$qr_code )) {
             return response()->json(["message" => "etudiant edited success"], 200);
--- app/Metier/EtudiantRepository.php
+++ app/Metier/EtudiantRepository.php
@@ -1,30 +1,63 @@
 <?php
 
-/**
- * Created by IntelliJ IDEA.
- * User: Abbes
- * Date: 29/07/2017
- * Time: 12:03
- */
 
 namespace App\Metier;
 
-
 use App\Models\Etudiant;
-use App\Models\Hopital;
 
 class EtudiantRepository extends ResourceRepository
 {
 
-
-    public function __construct(Etudiant $etudiant)
+    public function __construct(Etudiant $session)
     {
-        $this->model = $etudiant;
+        $this->model = $session;
+    }
+
+    public function addEtudiant($niveauId,$competencesId,$nom,$prenom,$CIN,$carte_Etudiant,$email,$active,$confirmation_code,$qr_code)
+    {
+        $etudiant = new Etudiant();
+
+        $etudiant->niveauId = $niveauId;
+        $etudiant->competencesId = $competencesId;
+        $etudiant->nom = $nom;
+        $etudiant->prenom = $prenom;
+        $etudiant->CIN = $CIN;
+        $etudiant->carte_Etudiant = $carte_Etudiant;
+        $etudiant->email = $email;
+        $etudiant->active = $active;
+        $etudiant->confirmation_code = $confirmation_code;
+        $etudiant->qr_code = $qr_code;
+
+        return $etudiant->save();
     }
 
     public function isExistEtudiant($etudiantId)
     {
         return Etudiant::where("id_Etudiant", "=", $etudiantId)
             ->first();
+    }
+
+    public function deleteEtudiant($etudiantId)
+    {
+        return Etudiant::where("id_Etudiant", "=", $etudiantId)
+            ->delete();
+    }
+
+    public function updateEtudiant($etudiantId, $niveauId,$competencesId,$nom,$prenom,$CIN,$carte_Etudiant,$email,$active,$confirmation_code,$qr_code)
+    {
+        $etudiant = Etudiant::find($etudiantId);
+
+        $etudiant->niveauId = $niveauId;
+        $etudiant->competencesId = $competencesId;
+        $etudiant->nom = $nom;
+        $etudiant->prenom = $prenom;
+        $etudiant->CIN = $CIN;
+        $etudiant->carte_Etudiant = $carte_Etudiant;
+        $etudiant->email = $email;
+        $etudiant->active = $active;
+        $etudiant->confirmation_code = $confirmation_code;
+        $etudiant->qr_code = $qr_code;
+
+        return $etudiant->save();
     }
 }
